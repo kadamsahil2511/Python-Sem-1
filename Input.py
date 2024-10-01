e176db1fd3dcabef9ece8aa2c8c88fba1ec54f5c
kadamsahil2511 <kadamsahil2511@gmail.com>
2024-10-01 15:52:41 +0530

    added split finction

--- Input.py
+++ Input.py
@@ -4,3 +4,8 @@ print (type(b))
 x=input('enter first no : ')
 y=input('enter secon no : ')
 print(x+y)
+
+#Accept the email from the user and print user name and domain name
+username, domain=input("Enter your email : ").split("@")
+print("The username is",username)
+print("The domain name is", domain)
