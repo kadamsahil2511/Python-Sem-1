da5e5a4d6f0f15a4d13b1b09bfcdc99adad7ea6f
kadamsahil2511 <kadamsahil2511@gmail.com>
2024-10-04 15:51:29 +0530

    corrected the bug

--- Assessment/Ques_10.py
+++ Assessment/Ques_10.py
@@ -1,8 +1,8 @@
 #Accept the market price from the user and calculate the net amount as (marked price- discount) to pay according to the criteria
 markedprice=int(input("Enter the marked price of product : "))
 if markedprice<=7000:
-    print("The Price after discount is Rs.", 0.1*markedprice)
+    print("The Price after discount is Rs.", markedprice-0.1*markedprice)
 elif markedprice>7000 and markedprice<=10000:
-    print("The Price after discount is Rs.", 0.15*markedprice) 
+    print("The Price after discount is Rs.", markedprice-0.15*markedprice) 
 else :
-    print("The Price after discount is Rs.", 0.20*markedprice)
+    print("The Price after discount is Rs.", markedprice-0.20*markedprice)
