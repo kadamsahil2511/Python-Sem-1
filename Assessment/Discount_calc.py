caf85ebaa9e8ae7d11b48a2b610daf101230a8bd
kadamsahil2511 <kadamsahil2511@gmail.com>
2024-10-04 15:36:21 +0530

    updates wrong variables

--- Assessment/Discount_calc.py
+++ Assessment/Discount_calc.py
@@ -1,5 +1,5 @@
 #Accept the market price from the user and calculate the net amount as (marked price- discount) to pay according to the criteria
-markedprice=int(input("Enter the cost price of the bike : "))
+markedprice=int(input("Enter the marked price of product : "))
 if markedprice<=7000:
     print("The Price after discount is Rs.", 0.1*markedprice)
 elif markedprice>7000 and markedprice<=10000:
