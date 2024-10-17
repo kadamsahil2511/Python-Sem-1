550546e4f9da719bbb67e82bb221bfdd6505b017
kadamsahil2511 <kadamsahil2511@gmail.com>
2024-10-17 23:02:19 +0530

    added oct 17

--- October_17.py
+++ October_17.py
@@ -1,5 +1,8 @@
 # October_17
 # Python program to show that the variables with a value assigned in class declaration, are class variables
+'''
+from sys import argv
+
 
 class CSStudent:
     stream = 'IT' #Class Variable
@@ -33,3 +36,56 @@ print("b.stream",b.stream) #prints 'IT'
 #To change the stream for all instances of the class, we can change it directly from the class 
 CSStudent.stream = 'CS'
 
+#Different types of constructors in Python
+#1. Default constructor
+#2. Parameterized constructor
+
+class const_types:
+    def __init__(self):
+        print("Default Constructor")
+    def __init__(self,arg1=0):
+        data = arg1
+        print("Paramaterized Constructor")
+    def __init__(self,other):
+        self.data=other.data
+        print("Parametrised Construction")
+# obj1=const_types()
+# obj2=const_types(10)
+ 
+#Operations on attributes
+getattr(obj,name, default): #It is used to access the attribute of the object.
+setattr(obj, name, value):# It is used to set a particular value to the specific attribute of an object.
+delattr(obj, name): #It is used to delete a specific attribute.
+hasattr(obj, name):# It returns true if the object contains some specific attribute.
+ '''
+class student:
+    #Attributes (data)
+    def __init__(self):
+        self.roll=0
+        self.sname=''
+
+    #Methods
+    # def setdata(self):
+    #     self.roll=int(input("Enter your roll no : ")) 
+    #     self.sname=input("Enter your name: ")
+    def getdata(self):
+        print("Roll No: ",self.roll) 
+        print("Name : ",self.sname)
+
+s1=student()#object created
+# s1.setdata()
+ 
+# print(getattr(s1, 'sname'))
+# print(hasattr(s1, 'roll'))
+# setattr(s1, 'sname', 'Vijay')
+# print(getattr(s1, 'sname'))
+# print(hasattr(s1, 'sname'))
+# s1.getdata()
+# delattr(s1, 'sname')
+
+print(student.__doc__)
+print(student.__dict__)
+print(student.__name__)
+print(student.__module__)
+
+#Destructor
